2d87a70c49dd4c452b739d431e2ca567a20dee74
alexanderochoa <eochoa@csticorp.biz>
2022-12-20 23:54:46 -0500

    feat: Create API pagos servicios

---
+++ pagos_2/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from users_2.models import User
+
+class Services(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    logo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "services"
+
+
+class Payment_user(models.Model):
+    @property
+    def username(self):
+        return self.user_id.username
+
+    @property
+    def servicename(self):
+        return self.service_id.name
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    service_id = models.ForeignKey(Services, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    paymentDate = models.DateField()
+    expirationDate = models.DateField()
+    paymentDate = models.DateField()
+    expirationDate = models.DateField()
+
+    class Meta:
+        db_table = "payment_user"
+
+    def __str__(self):
+        return self.user_id.username + " - " + self.service_id.name
+
+
+class Expired_payments(models.Model):
+    @property
+    def username(self):
+        return self.payment_user_id.user_id.username
+
+    @property
+    def servicename(self):
+        return self.payment_user_id.service_id.name
+
+    payment_user_id = models.ForeignKey(Payment_user, on_delete=models.CASCADE)
+    penalty_fee_amount = models.IntegerField()
+
+    def user_id(self):
+        return self.payment_user_id.pk
+
+    def __str__(self):
+        return str(self.payment_user_id.user_id.username)
+
+    class Meta:
+        db_table = "expired_payments"
